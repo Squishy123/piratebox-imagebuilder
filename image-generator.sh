706c275ea9f9b50fd0e8ffda842e79e619e201e0
Christian Wang <christian.gnaw@gmail.com>
2017-09-09 00:09:41 -0400

    Fixed scripts

--- image-generator.sh
+++ image-generator.sh
@@ -7,7 +7,7 @@ echo "WELCOME TO THE PIRATEBOX IMAGE-GENERATOR"
 
 while true; do
 
-  read -n1 -r -p "$(echo -e 'Choose option 1. for snapshot build\n Choose option 2. for stable build\n Choose option 3. to quit\n\b')" option
+  read -n1 -r -p "$(echo -e 'Choose option 1. for snapshot build\n Choose option 2. for stable build\n Choose option 3. to clean files\n Choose option 4. to quit\n\b')" option
   case $option in
     [1]* ) echo "Installing Packages..."
     sed -i -e 's/false/true/g' lede/Makefile
@@ -17,7 +17,12 @@ while true; do
     sed -i -e 's/true/false/g' lede/Makefile
     echo "\n"
     break;;
-    [3]* ) exit break;;
+    [3]* )
+    cd lede
+    make distclean
+    cd ..
+    exit break;;
+    [4]* ) exit break;;
     *) echo "Please choose an option"
   esac
 done
@@ -32,4 +37,4 @@ read profile
 echo "Attempting to build the image"
 make $profile
 
-echo "All done, the image can be found in the target__ar71xx folder"
+echo "All done, the image can be found in the target__ar71xx-generic folder"
